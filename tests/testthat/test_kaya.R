7360297835de3b650a57be999b2361fb8983df20
Luyang Chu <102659791+luyangchu@users.noreply.github.com>
2022-10-27 19:54:54 +0200

    change tests

--- tests/testthat/test_kaya.R
+++ tests/testthat/test_kaya.R
@@ -1,7 +1,7 @@
 library("Assignment")
 
 # Test the function returns the correct results
-test_that("kaya equation returns the correct results",{
+test_that("kaya_identity_equation returns the correct results",{
   expect_equal(kaya_identity_equation(20,30,6,0.02), 72)
   expect_equal(kaya_identity_equation(300,50,7,0.08), 8400)
   expect_equal(kaya_identity_equation(3,5,3,0), 0)
@@ -9,7 +9,7 @@ test_that("kaya equation returns the correct results",{
 })
 
 # Test the function with negative inputs.
-test_that("kaya equation with negative inputs", {
+test_that("kaya_identity_equation with negative inputs", {
   expect_error(kaya_identity_equation(20,-30,6,0.02),
                "Assertion on 'gdp' failed: Element 1 is not >= 0.")
   expect_error(kaya_identity_equation(300,50,7,-3),
@@ -20,9 +20,9 @@ test_that("kaya equation with negative inputs", {
 })
 
 # Test the function  with two output types.
-test_that("kaya equation returns the correct output tpe",{
+test_that("kaya_identity_equation returns the correct output type",{
   expect_equal(kaya_identity_equation(20,30,6,0.02,"CO2"), 72)
   expect_equal(kaya_identity_equation(367,50,7,1,"C"), 35000)
-  expect_equal(kaya_identity_equation(3,5,3,0,"C"), 0)
+  expect_equal(kaya_identity_equation(3,5,3,0.367,"C"), 4.5)
 
 })
