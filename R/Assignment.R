ef74a8cdfc8a572279684d051a4a14419b7a9501
Luyang Chu <102659791+luyangchu@users.noreply.github.com>
2022-10-27 17:30:07 +0200

    #  new files

---
+++ R/Assignment.R
@@ -0,0 +1,8 @@
+#' Kaya Identity Package
+#'
+#' contains one function kaya Identity which expresses yearly CO2 emissions
+#'
+#' @docType package
+#' @author Luyang Chu \email{luyang.chu@gmail.com}
+#' @name assignment
+NULL
---
+++ R/kaya_identity_equation.R
@@ -0,0 +1,36 @@
+library(checkmate)
+library(roxygen2)
+#' create Kaya equation as a function
+#'
+#' more detailed description.
+#'
+#' @param pop Numeric
+#' @param gdp Numeric
+#' @param enInt Numeric
+#' @param carnInt Numeric
+#'
+#' @return Numeric
+#'
+#' @examples
+#' kaya_equation.germany <- kaya_equation(82.4, 44, 5, 0.05)
+#'
+#' @export
+#'
+kaya_equation <- function(pop,gdp,enInt,carbInt,output_type = "CO2"){
+  # all the variables are non negative
+  # add checkmate to check input
+  checkmate::assertNumber(pop, na.ok = FALSE, lower = 0)
+  checkmate::assertNumber(gdp, na.ok = FALSE, lower = 0)
+  checkmate::assertNumber(enInt, na.ok = FALSE, lower = 0)
+  checkmate::assertNumber(carbInt, na.ok = FALSE, lower = 0)
+  yearly_CO2 <-  pop * gdp * enInt * carbInt
+
+  # two output types, CO2 and C, default CO2
+  # 3.67 tonnes of CO2 equals 1 tonne of C
+  if (output_type == "C") {
+    yearly_C <- yearly_CO2 / 3.67
+    return(yearly_C)
+  }else {
+    return(yearly_CO2)
+  }}
+
